000ae4a61d6a40e10efe6b61e12405af76c5bc20
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2023-11-26 19:15:58 +0400

    Страница Project на трех компонентах

--- interno-project/src/App.vue
+++ interno-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Страница MyHeader</h1>
+    <!-- <h1>Страница MyHeader</h1>
     <MyHeader />
     <h1>Страница MyFooter</h1>
     <MyFooter />
@@ -9,25 +9,29 @@
     <h1>Страница MyBlog</h1>
     <MyBlog />
     <h1>Страница MyBlogDetails</h1>
-    <MyBlogDetails />
+    <MyBlogDetails /> -->
+    <h1>Страница Project</h1>
+    <MyProject />
   </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader.vue'
-import MyFooter from './components/MyFooter.vue'
-import MyIndex from './components/MyIndex.vue'
-import MyBlog from './components/MyBlog.vue'
-import MyBlogDetails from './components/MyBlogDetails.vue'
+// import MyHeader from './components/MyHeader.vue'
+// import MyFooter from './components/MyFooter.vue'
+// import MyIndex from './components/MyIndex.vue'
+// import MyBlog from './components/MyBlog.vue'
+// import MyBlogDetails from './components/MyBlogDetails.vue'
+import MyProject from './components/MyProject/MyProject.vue'
 
 export default {
   name: 'App',
   components: {
-    MyHeader,
-    MyFooter,
-    MyIndex,
-    MyBlog,
-    MyBlogDetails
+    // MyHeader,
+    // MyFooter,
+    // MyIndex,
+    // MyBlog,
+    // MyBlogDetails,
+    MyProject
   }
 }
 </script>
---
+++ interno-project/src/components/MyProject/ExampleItem.vue
@@ -0,0 +1,50 @@
+<template>
+      <div class="examples__Item">
+        <img :src="item.url" alt="examples__image" class="examples__image" />
+        <h2 class="title">{{ item.title }}:{{ item.id }}</h2>
+        <div class="dateBox">
+          <p class="dateBoxText">{{ item.text }}</p>
+          <a href="#">
+            <svg
+              class="dateBoxImage"
+              xmlns="http://www.w3.org/2000/svg"
+              width="52"
+              height="53"
+              viewBox="0 0 52 53"
+              fill="none"
+            >
+              <circle
+                class="circle"
+                cx="26"
+                cy="26.2671"
+                r="26"
+                fill="#F4F0EC"
+              />
+              <path
+                d="M23.7714 32.9529L29.7142 26.2672L23.7714 19.5815"
+                stroke="#292F36"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleItem',
+  props: ['item'],
+  data () {
+    return {}
+  },
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
