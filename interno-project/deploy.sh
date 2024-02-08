3118a6777de42383be28cbce98c800b676c16da9
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2024-02-08 12:04:23 +0400

    Dist

--- interno-project/deploy.sh
+++ interno-project/deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<INTERNO.JS>/<interno-project>.git master:gh-pages
 
 cd -
