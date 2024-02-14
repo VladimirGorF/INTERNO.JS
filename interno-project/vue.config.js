7001c4953eb0133681d74122ccdaf87adeacc0e0
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2024-02-14 20:03:37 +0400

    end

--- interno-project/vue.config.js
+++ interno-project/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/INTERNO.JS/'
+  transpileDependencies: true
+  // publicPath: '/INTERNO.JS/'
 })
